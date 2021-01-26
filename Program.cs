fc8f3c8235d284c8b8ec3f5b7eee2f845cef755b
Nikita Grishaev <grishaev@rightway.games>
2021-01-26 10:05:22 +0300

    Task1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿namespace AlgoP3
+﻿using System;
+
+namespace AlgoP3
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var arr = new[] {1,4, 2, 3};
+
+            foreach (var i in arr)
+            {
+                Console.Write(i + " ");
+            }
         }
     }
 }
