56b7923cab83e67ba0b1453f2d1bbb484f5067ad
Unknown <lojso.gm@gmail.com>
2021-01-30 13:15:43 +0300

    Task 3 ShellSort

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using SortSpace;
 
 namespace AlgoP3
 {
@@ -6,7 +7,14 @@ namespace AlgoP3
     {
         public static void Main(string[] args)
         {
-            var arr = new[] {1,4, 2, 3};
+            var arr = new[] {1,6,5,4,3};
+            var arr2 = new[] {2, 1, 3};
+            var arr3 = new[] {87, 17, 24, 61, 71, 78, 72, 30, 2, 35, 64, 38, 86};
+            var arr4 = new[] {87, 17};
+            var arr5 = new[] {87};
+
+            var res = SortLevel.KnuthSequence(1);
+            SortLevel.ShellSort(arr5);
 
             foreach (var i in arr)
             {
--- Tasks/Task3.cs
+++ Tasks/Task3.cs
@@ -5,7 +5,101 @@ namespace SortSpace
 {
     public static class SortLevel
     {
-        public static List<int> KnuthSequence( int array_size )
+        public static void ShellSort(int[] arr)
+        {
+            var knuthSequence = KnuthSequence(arr.Length);
+            for (int i = 0; i < knuthSequence.Count; i++)
+            {
+                var step = knuthSequence[i];
+                for (int j = 0; j < step; j++)
+                {
+                    InsertionSortStep(arr, step, i);
+                }
+            }
+        }
+        
+        public static void InsertionSortStep(int[] array, int step, int i)
+        {
+            var stepArray = new int[(int) Math.Ceiling((float) (array.Length - i) / step)];
+            for (int j = i, k = 0; j < array.Length; j += step, k++)
+            {
+                stepArray[k] = array[j];
+            }
+
+            SimpleInsertionStep(stepArray);
+
+            for (int j = i, k = 0; j < array.Length; j += step, k++)
+            {
+                array[j] = stepArray[k];
+            }
+        }
+
+        public static void SimpleInsertionStep(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                var positionToInsertion = FindPositionToInsert(array, i);
+                InsertElement(array, positionToInsertion, i);
+            }
+        }
+
+        private static int FindPositionToInsert(int[] array, int index)
+        {
+            var result = index;
+            for (int i = index - 1; i >= 0; i--)
+            {
+                if (array[i] < array[index])
+                    return i + 1;
+                result = i;
+            }
+
+            return result;
+        }
+
+        private static void InsertElement(int[] array, int insertionIndex, int elementIndex)
+        {
+            if (insertionIndex < elementIndex)
+            {
+                InsertLeft(array, insertionIndex, elementIndex);
+            }
+            else if (insertionIndex > elementIndex)
+            {
+                InsertRight(array, insertionIndex, elementIndex);
+            }
+        }
+
+        private static void InsertRight(int[] array, int insertionIndex, int elementIndex)
+        {
+            var element = array[elementIndex];
+
+            for (int i = elementIndex + 1; i <= insertionIndex; i++)
+            {
+                array[i] = array[i + 1];
+            }
+
+            array[insertionIndex] = element;
+        }
+
+        private static void InsertLeft(int[] array, int insertionIndex, int elementIndex)
+        {
+            var element = array[elementIndex];
+
+            for (int i = elementIndex; i > insertionIndex; i--)
+            {
+                array[i] = array[i - 1];
+            }
+
+            array[insertionIndex] = element;
+        }
+
+        private static void Swap(int[] array, int i, int minElementIndex)
+        {
+            var temp = array[i];
+            array[i] = array[minElementIndex];
+            array[minElementIndex] = temp;
+        }
+
+        public static List<int> KnuthSequence(int array_size)
         {
             var seq = new List<int>();
             for (int i = 1; i <= array_size; i = NextElement(i))
