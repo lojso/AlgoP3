bff0cf8456753aca4b42ee179afc782171c5c823
Unknown <lojso.gm@gmail.com>
2021-02-06 15:09:46 +0300

    Task 5

--- Tasks/Task4.cs
+++ Tasks/Task4.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace SortSpace
 {
-    public static class SortLevel
+    public static class Task4
     {
         public static int ArrayChunk(int[] M)
         {
---
+++ Tasks/Task5.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortSpace
+{
+    public static class SortLevel
+    {
+        public static void QuickSort( int[] array, int left, int right)
+        {
+            if(left >= right)
+                return;
+
+            var baseElement = ArrayChunk(array, left, right);
+            QuickSort(array, left, baseElement - 1);
+            QuickSort(array, baseElement, right);
+        }
+        
+        public static int ArrayChunk(int[] M, int start, int end)
+        {
+            while (true)
+            {
+                var NIndex = (start + end + 1) / 2;
+                var N = M[NIndex];
+                var i = start;
+                var j = end;
+                while (true)
+                {
+                    while (M[i] < N)
+                    {
+                        i++;
+                    }
+
+                    while (M[j] > N)
+                    {
+                        j--;
+                    }
+
+                    if (i == j - 1 && M[i] > M[j])
+                    {
+                        Swap(M, i, j);
+                        break;
+                    }
+
+                    if (i == j || (i == j - 1 && M[i] < M[j]))
+                    {
+                        return NIndex;
+                    }
+
+                    Swap(M, i, j);
+                    
+                    if (M[i] == N)
+                    {
+                        NIndex = i;
+                    }
+                    
+                    if (M[j] == N)
+                    {
+                        NIndex = j;
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
