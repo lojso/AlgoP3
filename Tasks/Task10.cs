406c8f4f6f09508bf1422acb9c851d2d2d7356ec
Nikita Grishaev <grishaev@rightway.games>
2021-03-07 02:29:40 +0300

    Task 10 template

---
+++ Tasks/Task10.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortSpace
+{
+    public class ksort
+    {
+        public string[] items;
+        
+        public ksort()
+        {
+            
+        }
+
+        public int index(string s)
+        {
+            return 0;
+        }
+
+        public bool add(string s)
+        {
+            return false;
+        }
+    }
+}
