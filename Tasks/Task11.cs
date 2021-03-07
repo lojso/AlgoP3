e6dd92b959505f31dd210a8dc50ac477e7a3d2a7
Nikita Grishaev <grishaev@rightway.games>
2021-03-07 22:23:39 +0300

    Task 11 12 templates

---
+++ Tasks/Task11.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortSpace
+{
+    public class BinarySearch
+    {
+        public int Left;
+        public int Right;
+
+        public BinarySearch(int[] array)
+        {
+            Left = 0;
+            Right = array.Length - 1;
+        }
+
+        public void Step(int N)
+        {
+            
+        }
+
+        public int GetResult()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Tasks/Task12.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortSpace
+{
+    public static class SortLevel
+    {
+        public static bool GallopingSearch(int[] array, int value)
+        {
+            return false;
+        }
+    }
+}
