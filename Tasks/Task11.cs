56f1cecbde5c496c337c93556a7ac471b48f8340
Unknown <lojso.gm@gmail.com>
2021-03-12 23:52:10 +0300

    Task 11

--- Tasks/Task11.cs
+++ Tasks/Task11.cs
@@ -8,19 +8,46 @@ namespace SortSpace
         public int Left;
         public int Right;
 
+        private readonly int[] _array;
+        private bool _wasElementFound;
+
         public BinarySearch(int[] array)
         {
+            _array = array;
             Left = 0;
             Right = array.Length - 1;
+            _wasElementFound = false;
         }
 
         public void Step(int N)
         {
+            if(Left > Right)
+                return;
             
+            var mid = (Left + Right) / 2;
+
+            if (N < _array[mid])
+            {
+                Right = mid;
+            }
+            else if (N < _array[mid])
+            {
+                Left = mid + 1;
+            }
+            else
+            {
+                _wasElementFound = true;
+            }
         }
 
         public int GetResult()
         {
+            if (_wasElementFound)
+                return 1;
+
+            if (Left > Right)
+                return -1;
+            
             return 0;
         }
     }
