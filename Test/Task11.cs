3d65100627e19b3aff1fa24ceb92e67c2413df9f
Nikita Grishaev <grishaev@rightway.games>
2021-03-16 10:26:37 +0300

    Tests for Task11

---
+++ Test/Task11.cs
@@ -0,0 +1,173 @@
+﻿using NUnit.Framework;
+using SortSpace;
+
+namespace Test
+{
+    [TestFixture]
+    public class Task11
+    {
+        [Test]
+        public void Array_123_Search2()
+        {
+            var bsearch = new BinarySearch(new int[]{1, 2, 3});
+             
+            bsearch.Step(2);
+            
+            Assert.True(bsearch.GetResult() == 1);
+            Assert.True(bsearch.Left == 0);
+            Assert.True(bsearch.Right == 2);
+        }
+        
+        [Test]
+        public void Array_123_Search2_Oversearch()
+        {
+            var bsearch = new BinarySearch(new int[]{1, 2, 3});
+             
+            bsearch.Step(2);
+            bsearch.Step(2);
+            bsearch.Step(2);
+
+            Assert.True(bsearch.GetResult() == 1);
+            Assert.True(bsearch.Left == 0);
+            Assert.True(bsearch.Right == 2);
+        }
+        
+        [Test]
+        public void Array_123_Search1()
+        {
+            var bsearch = new BinarySearch(new int[]{1, 2, 3});
+             
+            bsearch.Step(1);
+
+            Assert.True(bsearch.GetResult() == 0);
+            Assert.True(bsearch.Left == 0);
+            Assert.True(bsearch.Right == 0);
+            
+            bsearch.Step(1);
+            
+            Assert.True(bsearch.GetResult() == 1);
+            Assert.True(bsearch.Left == 0);
+            Assert.True(bsearch.Right == 0);
+        }
+        
+        [Test]
+        public void Array_123_Search3()
+        {
+            var bsearch = new BinarySearch(new int[]{1, 2, 3});
+             
+            bsearch.Step(3);
+            
+            Assert.True(bsearch.GetResult() == 0);
+            Assert.True(bsearch.Left == 2);
+            Assert.True(bsearch.Right == 2);
+            
+            bsearch.Step(3);
+            
+            Assert.True(bsearch.GetResult() == 1);
+            Assert.True(bsearch.Left == 2);
+            Assert.True(bsearch.Right == 2);
+        }
+
+        [Test]
+        public void Array_123_Search4()
+        {
+            var bsearch = new BinarySearch(new int[]{1, 2, 3});
+             
+            bsearch.Step(4);
+            
+            Assert.True(bsearch.GetResult() == 0);
+            Assert.True(bsearch.Left == 2);
+            Assert.True(bsearch.Right == 2);
+            
+            bsearch.Step(4);
+            
+            Assert.True(bsearch.GetResult() == -1);
+            Assert.True(bsearch.Left == 3);
+            Assert.True(bsearch.Right == 2);
+        }
+
+        [Test]
+        public void Array_123_Search0()
+        {
+            var bsearch = new BinarySearch(new int[]{1, 2, 3});
+             
+            bsearch.Step(0);
+            
+            Assert.True(bsearch.GetResult() == 0);
+            Assert.True(bsearch.Left == 0);
+            Assert.True(bsearch.Right == 0);
+            
+            bsearch.Step(0);
+            
+            Assert.True(bsearch.GetResult() == -1);
+            Assert.True(bsearch.Left == 0);
+            Assert.True(bsearch.Right == -1);
+        }
+        
+        [Test]
+        public void Array_1_Search1()
+        {
+            var bsearch = new BinarySearch(new int[]{1});
+             
+            bsearch.Step(1);
+            
+            Assert.True(bsearch.GetResult() == 1);
+            Assert.True(bsearch.Left == 0);
+            Assert.True(bsearch.Right == 0);
+        }
+        
+        [Test]
+        public void Array_1_Search0()
+        {
+            var bsearch = new BinarySearch(new int[]{1});
+             
+            bsearch.Step(0);
+            
+            Assert.True(bsearch.GetResult() == -1);
+            Assert.True(bsearch.Left == 0);
+            Assert.True(bsearch.Right == -1);
+        }
+        
+        [Test]
+        public void Array_Empty_Search0()
+        {
+            var bsearch = new BinarySearch(new int[]{});
+             
+            bsearch.Step(0);
+            
+            Assert.True(bsearch.GetResult() == -1);
+            Assert.True(bsearch.Left == 0);
+            Assert.True(bsearch.Right == -1);
+        }
+        
+        [Test]
+        public void Array_12_Search1()
+        {
+            var bsearch = new BinarySearch(new int[]{1, 2});
+             
+            bsearch.Step(1);
+            
+            Assert.True(bsearch.GetResult() == 1);
+            Assert.True(bsearch.Left == 0);
+            Assert.True(bsearch.Right == 1);
+        }
+        
+        [Test]
+        public void Array_12_Search2()
+        {
+            var bsearch = new BinarySearch(new int[]{1, 2});
+             
+            bsearch.Step(2);
+            
+            Assert.True(bsearch.GetResult() == 0);
+            Assert.True(bsearch.Left == 1);
+            Assert.True(bsearch.Right == 1);
+            
+            bsearch.Step(2);
+            
+            Assert.True(bsearch.GetResult() == 1);
+            Assert.True(bsearch.Left == 1);
+            Assert.True(bsearch.Right == 1);
+        }
+    }
+}
